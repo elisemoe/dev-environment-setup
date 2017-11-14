8082af5c871a9e5a858769da5e9f8ded0f57b60c
Elise Hollowed <emhollowed@gmail.com>
2017-11-14 15:55:04 -0800

    init repo + powershell setup script

---
+++ setup.ps1
@@ -0,0 +1,43 @@
+# Base Applications
+choco install -y googlechrome
+choco install -y skype
+choco install -y notepadplusplus
+choco install -y slack
+choco install -y f.lux
+choco install -y greenshot
+
+# Dev Environment Applications
+choco install -y conemu
+choco install -y git
+choco install -y putty
+choco install -y python
+choco install -y rocketchat
+choco install -y beyondcompare
+
+choco install -y visualstudio2017community
+choco install -y visualstudio2017buildtools
+choco install -y visualstudiocode
+
+choco install -y docker-for-windows
+choco install -y docker-compose
+
+# Command line utilities
+# Linux-like environment on Windows
+choco install -y nano
+choco install -y sudo
+choco install -y curl
+choco install -y vim
+choco install -y pip
+choco install -y openssh
+choco install -y wget
+choco install -y ack
+
+# choco install unity
+# choco install nmap
+# choco install mongodb
+# choco install elasticsearch
+# choco install nginx
+
+
+
+
